c761a1311be65f4841bf8de750aaef2c7536cbeb
Christopher Dresel <christopher.dresel@spatial-focus.net>
2017-07-10 13:04:30 +0200

    Added sample project.

---
+++ test/Automaty.Generators.EFCoreRepositories.Test/SampleTest.cs
@@ -0,0 +1,45 @@
+namespace Automaty.Generators.EFCoreRepositories.Test
+{
+	using System.Collections.Generic;
+	using Automaty.Core;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class SampleTest
+	{
+		protected const string ProjectDirectoryPath = "./samples/Automaty.Generators.EFCoreRepositories.Sample/";
+
+		protected const string ProjectFilePath = "Automaty.Generators.EFCoreRepositories.Sample.csproj";
+
+		[TestMethod]
+		public void AutomatyHelloWorldGenerateFiles()
+		{
+			string sampleProjectDirectoryPath = SampleTest.ProjectDirectoryPath.ToPlatformSpecificPath();
+			string projectFilePath = SampleTest.ProjectFilePath;
+
+			string sourceFilePath = "Repositories.cs";
+
+			ICollection<string> generatedFilePaths = new[]
+			{
+				"Repositories.Album.generated.cs", "Repositories.Artist.generated.cs", "Repositories.CartItem.generated.cs",
+				"Repositories.Genre.generated.cs", "Repositories.Music.generated.cs", "Repositories.Order.generated.cs",
+				"Repositories.OrderDetail.generated.cs"
+			};
+
+			Helper.AssertSampleProjectDirectoryPathExists(sampleProjectDirectoryPath);
+
+			foreach (string generatedFilePath in generatedFilePaths)
+			{
+				Helper.AssertGeneratedFileDoesNotExist(sampleProjectDirectoryPath, generatedFilePath);
+			}
+
+			Helper.DotNetRestore(sampleProjectDirectoryPath, projectFilePath);
+			Helper.AutomatyRun(sampleProjectDirectoryPath, $"{sourceFilePath} --project {projectFilePath}");
+
+			foreach (string generatedFilePath in generatedFilePaths)
+			{
+				Helper.AssertGeneratedFileExists(sampleProjectDirectoryPath, generatedFilePath);
+			}
+		}
+	}
+}
